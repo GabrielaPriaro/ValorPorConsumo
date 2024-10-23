4cdfd873afaf0262620e53cb75ce39e4414b8efa
Gabriela <gabriela.priaro@gmail.com>
2024-10-23 13:10:39 +0100

    Implementaçao para calcular valor a pagar se houver minutos excedidos.

--- ValorPorConsumo/Program.cs
+++ ValorPorConsumo/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace ValorPorConsumo
+﻿//Problema "operadora"
+//Uma operadora de telefonia cobra R$ 50.00 por um plano básico que dá direito a 100 minutos de
+//telefone. Cada minuto que exceder a franquia de 100 minutos custa R$ 2.00. Fazer um programa para
+//ler a quantidade de minutos que uma pessoa consumiu, daí mostrar o valor a ser pago. 
+
+using System.Globalization;
+
+namespace ValorPorConsumo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int quantMinutos, minutosExcedidos, valorTotal;
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Digite a quantidade de minutos: ");
+            quantMinutos = int.Parse(Console.ReadLine());
+
+            if (quantMinutos <= 100)
+            {
+                Console.WriteLine("Valor a pagar: R$ 50.00");
+            }
+            else 
+            {
+                minutosExcedidos = quantMinutos - 100;
+                valorTotal = minutosExcedidos * 2 + 50;
+                Console.WriteLine($"Valor a pagar: R$ {valorTotal.ToString("F2", CI)}");
+            }
         }
     }
 }
